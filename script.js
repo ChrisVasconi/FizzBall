999dd2cee4afb142c5ab342b342a5d54bfe54ee0
Chris Vasconi <vasconichris@gmail.com>
2022-05-30 18:54:45 -0400

    FizzBuzz

---
+++ script.js
@@ -0,0 +1,13 @@
+for(var x=1; x<100; x++) {
+    if(x % 3);{
+        console.log("fizz");
+    }
+    if (x % 5);
+    console.log("buzz"); {
+
+    }
+    if (x % 3,5); {
+        console.log("fizzbuzz");
+    }
+    console.log(x);
+}
